d8d3612cca40e33cd7f3f14af7ec4476554865b5
小白 <2806933146@qq.com>
2021-05-11 10:52:36 +0800

    第一版

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+    public Transform startTrans;    //起始点
+    LineRenderer lineRenderer;
+    // Use this for initialization
+    void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        lineRenderer.SetPosition(0, startTrans.position);
+        lineRenderer.SetPosition(1, transform.position);
+    }
+}
